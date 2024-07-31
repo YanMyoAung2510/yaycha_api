a92dd69966d39146c279dad3372319c0e8e57c5b
YanMyoAung2510 <gnext.yanmyoaung@gmail.com>
2024-08-01 01:14:44 +0630

    posts route and dynamic single post route

--- routers/content.js
+++ routers/content.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const prisma = require("../prismaClient");
+
 router.get("/posts", async (req, res) => {
   try {
     const data = await prisma.post.findMany({
       include: {
-        user: true,
+        users: true,
         comments: true,
       },
       orderBy: { id: "desc" },
@@ -16,4 +17,23 @@ router.get("/posts", async (req, res) => {
     res.status(500).json({ error: e });
   }
 });
+
+router.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await prisma.post.findFirst({
+      where: { id: Number(id) },
+      include: {
+        users: true,
+        comments: {
+          include: { users: true },
+        },
+      },
+    });
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+
 module.exports = { contentRouter: router };
